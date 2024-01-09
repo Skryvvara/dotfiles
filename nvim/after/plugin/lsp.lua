9901dd1b4c822b566513157a80b636c8aaa9f19c
Skryvvara <darkyne.gaming@outlook.de>
2024-01-09 09:55:29 +0100

    feat: added bashls to nvim lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,6 +7,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
     ensure_installed = {
         'clangd',
+        'bashls',
         'tsserver',
         'eslint',
         'lua_ls',
