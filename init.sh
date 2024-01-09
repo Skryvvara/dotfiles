52c739c2459967f85f7693bda4c2992b6dc8931e
Skryvvara <darkyne.gaming@outlook.de>
2024-01-09 09:47:21 +0100

    feat: improved init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 
 # Ask Y/n
 function ask() {
-    read -p "$1 (Y/n): " resp
+    read -r -p "$1 (Y/n): " resp
     if [ -z "$resp" ]; then
         response_lc="y" # empty is Yes
     else
@@ -18,38 +18,38 @@ function check_link() {
 }
 
 function is_installed() {
-  [ -x "$(command -v $1)" ]
+  [ -x "$(command -v "$1")" ]
 }
 
 # DEBUG HELPERS
 function error() {
-  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
 }
 
 function success() {
-  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
 }
 
 function info() {
-  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+  printf "\r  [ \033[00;34m..\033[0m ] %s\n" "$1"
 }
 
-function user() {
-  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+function warning() {
+  printf "\r  [\033[0;33mWARN\033[0m] %s\n" "$1"
 }
 
 path=$(eval echo ~/.config/nvim)
-if ask "Do you want to install neovim config ($path)?"; then
+if ask "Do you want to install neovim config (--> $path)?"; then
   if is_installed nvim; then
-    success "nvim is installed"
+    success "nvim is installed."
   else
-    brew install neovim
+    warning "nvim is not installed."
   fi
 
   if check_link "$path"; then
-    error "Error: $path already exists."
+    warning "$path already exists."
   else
-	  ln -s "$(realpath "nvim")" $path
+	  ln -s "$(realpath "nvim")" "$path"
     success "installed nvim config."
   fi
 fi
